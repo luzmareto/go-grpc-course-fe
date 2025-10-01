9ac00dc13c621b1dd9f44c359a876b05a8728b92
Luzmareto Rusmadian <900179@hq.bni.co.id>
2025-10-01 16:00:09 +0700

    inisiasi

--- src/components/ProductForm/ProductForm.tsx
+++ src/components/ProductForm/ProductForm.tsx
@@ -1,29 +1,80 @@
+import { useForm } from "react-hook-form"
+import FormInput from "../FormInput/FormInput";
+import * as yup from 'yup';
+import { yupResolver } from "@hookform/resolvers/yup";
+
+const createProductSchema = yup.object().shape({
+    name: yup.string().required("Nama produk wajib diisi"),
+    // price: yup.number().required("Harga produk wajib diisi").typeError("Harga produk tidak valid").moreThan(0, "Harga produk harus lebih dari 0"),
+    description: yup.string(),
+    image:yup.mixed<FileList>().required("Gambar produk wajib diisi")
+        .test("fileLength", "Gambar produk wajib diisi", (fileList) => {
+            return fileList.length > 0
+        })
+        .test("fileType", "Format gambar tidak valid", (fileList) => {
+            return fileList && fileList.length > 0 ? ["image/jpeg", "image/png"].includes(fileList[0].type) : true
+        })
+})
+
+
+interface ProductFormValues {
+    name: string;
+    // price: number;
+    description?: string;
+    image: FileList;
+}
+
 function ProductForm() {
+    const form = useForm<ProductFormValues>({
+        resolver: yupResolver(createProductSchema)
+    });
+
+    const submitHandler = (values: ProductFormValues) => {
+        console.log(values)
+    }
+
     return (
         <div className="p-4 p-lg-5 border bg-white">
-            <div className="form-group mb-3">
-                <label className="text-black" htmlFor="product_name">Nama Produk <span className="text-danger">*</span></label>
-                <input type="text" className="form-control" id="product_name" placeholder="Nama Produk" />
-            </div>
+            <form onSubmit={form.handleSubmit(submitHandler)} >
+               <FormInput<ProductFormValues>
+                    errors={form.formState.errors}
+                    name="name"
+                    register={form.register}
+                    type="text"
+                    label="Nama Produk"
+                    placeholder="Nama Produk"
+                    labelRequired
+               />
 
-            <div className="form-group mb-3">
-                <label className="text-black" htmlFor="product_price">Harga <span className="text-danger">*</span></label>
-                <input type="number" className="form-control" id="product_price" placeholder="Harga Produk" />
-            </div>
+                <div className="form-group mb-3">
+                    <label className="text-black" htmlFor="product_price">Harga <span className="text-danger">*</span></label>
+                    <input type="number" className="form-control" id="product_price" placeholder="Harga Produk" />
+                </div>
 
-            <div className="form-group mb-3">
-                <label className="text-black" htmlFor="product_image">Gambar Produk <span className="text-danger">*</span></label>
-                <input type="file" className="form-control" id="product_image" accept="image/*" />
-            </div>
+                <FormInput<ProductFormValues>
+                    errors={form.formState.errors}
+                    name="image"
+                    register={form.register}
+                    type="image"
+                    label="Gambar Produk"
+                    placeholder="Gambar Produk"
+                    labelRequired
+               />
 
-            <div className="form-group mb-4">
-                <label className="text-black" htmlFor="product_description">Deskripsi</label>
-                <textarea className="form-control" id="product_description" rows={4} placeholder="Deskripsi produk..."></textarea>
-            </div>
+                <FormInput<ProductFormValues>
+                    errors={form.formState.errors}
+                    name="description"
+                    register={form.register}
+                    type="textarea"
+                    label="Deskripsi"
+                    placeholder="Deskripsi Produk..."
+               />
 
-            <div className="form-group">
-                <button className="btn btn-primary">Simpan Produk</button>
-            </div>
+                <div className="form-group">
+                    <button className="btn btn-primary" type="submit">Simpan Produk</button>
+                </div>
+            </form>
+            
         </div>
     )
 }
