1b24b04007afdd4a66a91138c991f40fd8b6252e
Luzmareto Rusmadian <900179@hq.bni.co.id>
2025-10-10 10:36:14 +0700

    highlight 3 product teratas di halaman home

--- src/components/ProductHighlightSection/ProductHighlightSection.tsx
+++ src/components/ProductHighlightSection/ProductHighlightSection.tsx
@@ -1,10 +1,39 @@
 import { Link } from 'react-router-dom';
+import useGrpcApi from '../../hooks/useGrpcApi';
+import { useEffect, useState } from 'react';
+import { getProductClient } from '../../api/grpc/client';
+import { formatToIDR } from '../../utils/number';
+
+interface Product {
+    id: string
+    name: string
+    price: number;
+    imageUrl: string;
+}
 
 interface ProductHighlightSectionProps {
     beforeFooter?: boolean;
 }
 
 function ProductHighlightSection(props: ProductHighlightSectionProps) {
+    const [items, setItems] = useState<Product[]>([]);
+
+    const productApi = useGrpcApi();
+
+    useEffect (() => {
+        const fetchData = async () => {
+            const res = await productApi.callApi(getProductClient().highlightProducts({})); 
+        
+            setItems(res.response.data.map(d => ({
+                id: d.id,
+                imageUrl: d.imageUrl,
+                name: d.name,
+                price: d.price,
+            })))
+        }
+
+        fetchData();
+    }, [])
     return (
         <div className={`product-section ${props.beforeFooter ? 'before-footer-section' : ''}`}>
             <div className="container">
@@ -16,18 +45,21 @@ function ProductHighlightSection(props: ProductHighlightSectionProps) {
                     </div>
 
                     {/* Product Items */}
-                    <div className="col-12 col-md-4 col-lg-3 mb-5 mb-md-0">
-                        <Link className="product-item" to="/cart">
-                            <img src="/images/product-1.png" className="img-fluid product-thumbnail" alt="Nordic Chair" />
-                            <h3 className="product-title">Nordic Chair</h3>
-                            <strong className="product-price">Rp750.000</strong>
-                            <span className="icon-cross">
-                                <img src="/images/cross.svg" className="img-fluid" alt="Cross" />
-                            </span>
-                        </Link>
-                    </div>
+                    {items.map(item => (
+                        <div key={item.id} className="col-12 col-md-4 col-lg-3 mb-5 mb-md-0">
+                            <Link className="product-item" to="/cart">
+                                <img src={item.imageUrl}className="img-fluid product-thumbnail" alt="product_image" />
+                                <h3 className="product-title">{item.name}</h3>
+                                <strong className="product-price">{formatToIDR(item.price)}</strong>
+                                <span className="icon-cross">
+                                    <img src="/images/cross.svg" className="img-fluid" alt="Cross" />
+                                </span>
+                            </Link>
+                        </div>
 
-                    <div className="col-12 col-md-4 col-lg-3 mb-5 mb-md-0">
+                    ))}
+
+                    {/* <div className="col-12 col-md-4 col-lg-3 mb-5 mb-md-0">
                         <Link className="product-item" to="cart.html">
                             <img src="images/product-2.png" className="img-fluid product-thumbnail" />
                             <h3 className="product-title">Kruzo Aero Chair</h3>
@@ -49,7 +81,7 @@ function ProductHighlightSection(props: ProductHighlightSectionProps) {
                                 <img src="images/cross.svg" className="img-fluid" />
                             </span>
                         </Link>
-                    </div>
+                    </div> */}
                 </div>
             </div>
         </div>
