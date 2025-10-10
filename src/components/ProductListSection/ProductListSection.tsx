5f839a957cd0d8db67ea91bfb2a7bd5b17f5d52d
Luzmareto Rusmadian <900179@hq.bni.co.id>
2025-10-10 10:12:20 +0700

    list shop

--- src/components/ProductListSection/ProductListSection.tsx
+++ src/components/ProductListSection/ProductListSection.tsx
@@ -1,114 +1,65 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Pagination from "../Pagination/Pagination"
+import useGrpcApi from "../../hooks/useGrpcApi";
+import { getProductClient } from "../../api/grpc/client";
+import { formatToIDR } from "../../utils/number";
+
+interface Product {
+    id: string;
+    name: string;
+    price: number;
+    imageUrl: string;
+}
 
 function ProductListSection() {
+    const listApi = useGrpcApi();
     const [currentPage, setCurrentPage] = useState(1);
-    const totalPages = 5;
+    const [items, setItems] = useState<Product[]>([]);
+    const [totalPages, setTotalPages] = useState<number>(0);
 
     const handlePageChange = (page: number) => {
         setCurrentPage(page);
     };
+
+    useEffect (() => {
+        const fetchData = async () => {
+            const res = await listApi.callApi(getProductClient().listProduct({
+                pagination: {
+                    currentPage: currentPage,
+                    itemPerPage: 8,
+                }
+            }));
+
+            setItems(res.response.data.map(d => ({
+                id: d.id,
+                imageUrl: d.imageUrl,
+                name: d.name,
+                price: d.price,
+            })));
+            setTotalPages(res.response.pagination?.totalPageCount ?? 0);
+        }
+
+        fetchData();
+    }, [currentPage]);
 
     return (
         <div className="untree_co-section product-section before-footer-section">
             <div className="container">
                 <div className="row">
 
-                    <div className="col-12 col-md-4 col-lg-3 mb-5">
+                    {items.map(item => (
+                    <div key={item.id } className="col-12 col-md-4 col-lg-3 mb-5">
                         <a className="product-item" href="#">
-                            <img src="images/product-3.png" className="img-fluid product-thumbnail" />
-                            <h3 className="product-title">Kursi Nordic</h3>
-                            <strong className="product-price">Rp775.000</strong>
+                            <img src={item.imageUrl} className="img-fluid product-thumbnail" />
+                            <h3 className="product-title">{item.name}</h3>
+                            <strong className="product-price">{formatToIDR(item.price)}</strong>
 
                             <span className="icon-cross">
                                 <img src="images/cross.svg" className="img-fluid" />
                             </span>
                         </a>
                     </div>
-
-                    <div className="col-12 col-md-4 col-lg-3 mb-5">
-                        <a className="product-item" href="#">
-                            <img src="images/product-1.png" className="img-fluid product-thumbnail" />
-                            <h3 className="product-title">Kursi Nordic</h3>
-                            <strong className="product-price">Rp775.000</strong>
-
-                            <span className="icon-cross">
-                                <img src="images/cross.svg" className="img-fluid" />
-                            </span>
-                        </a>
-                    </div>
-
-                    <div className="col-12 col-md-4 col-lg-3 mb-5">
-                        <a className="product-item" href="#">
-                            <img src="images/product-2.png" className="img-fluid product-thumbnail" />
-                            <h3 className="product-title">Kursi Kruzo Aero</h3>
-                            <strong className="product-price">Rp1.209.000</strong>
-
-                            <span className="icon-cross">
-                                <img src="images/cross.svg" className="img-fluid" />
-                            </span>
-                        </a>
-                    </div>
-
-                    <div className="col-12 col-md-4 col-lg-3 mb-5">
-                        <a className="product-item" href="#">
-                            <img src="images/product-3.png" className="img-fluid product-thumbnail" />
-                            <h3 className="product-title">Kursi Ergonomis</h3>
-                            <strong className="product-price">Rp666.500</strong>
-
-                            <span className="icon-cross">
-                                <img src="images/cross.svg" className="img-fluid" />
-                            </span>
-                        </a>
-                    </div>
-
-                    <div className="col-12 col-md-4 col-lg-3 mb-5">
-                        <a className="product-item" href="#">
-                            <img src="images/product-3.png" className="img-fluid product-thumbnail" />
-                            <h3 className="product-title">Kursi Nordic</h3>
-                            <strong className="product-price">Rp775.000</strong>
-
-                            <span className="icon-cross">
-                                <img src="images/cross.svg" className="img-fluid" />
-                            </span>
-                        </a>
-                    </div>
-
-                    <div className="col-12 col-md-4 col-lg-3 mb-5">
-                        <a className="product-item" href="#">
-                            <img src="images/product-1.png" className="img-fluid product-thumbnail" />
-                            <h3 className="product-title">Kursi Nordic</h3>
-                            <strong className="product-price">Rp775.000</strong>
-
-                            <span className="icon-cross">
-                                <img src="images/cross.svg" className="img-fluid" />
-                            </span>
-                        </a>
-                    </div>
-
-                    <div className="col-12 col-md-4 col-lg-3 mb-5">
-                        <a className="product-item" href="#">
-                            <img src="images/product-2.png" className="img-fluid product-thumbnail" />
-                            <h3 className="product-title">Kursi Kruzo Aero</h3>
-                            <strong className="product-price">Rp1.209.000</strong>
-
-                            <span className="icon-cross">
-                                <img src="images/cross.svg" className="img-fluid" />
-                            </span>
-                        </a>
-                    </div>
-
-                    <div className="col-12 col-md-4 col-lg-3 mb-5">
-                        <a className="product-item" href="#">
-                            <img src="images/product-3.png" className="img-fluid product-thumbnail" />
-                            <h3 className="product-title">Kursi Ergonomis</h3>
-                            <strong className="product-price">Rp666.500</strong>
-
-                            <span className="icon-cross">
-                                <img src="images/cross.svg" className="img-fluid" />
-                            </span>
-                        </a>
-                    </div>
+                    ))}
 
                 </div>
 
