a9cabeda582349672618d780b50d2b461479ab08
Luzmareto Rusmadian <900179@hq.bni.co.id>
2025-11-11 15:07:56 +0700

    Integrasi Detail Order Admin FE

--- src/pages/AdminOrderDetail/AdminOrderDetail.tsx
+++ src/pages/AdminOrderDetail/AdminOrderDetail.tsx
@@ -1,13 +1,35 @@
-import { Link } from "react-router-dom"
+import { Link, useParams } from "react-router-dom"
+import useGrpcApi from "../../hooks/useGrpcApi"
+import { useEffect, useState } from "react";
+import { getOrderClient } from "../../api/grpc/client";
+import { DetailOrderRequest, DetailOrderResponse } from "../../../pb/order/order";
+import OrderStatusBadge from "../../components/OrderStatusBadge/OrderStatusBadge";
+import { converTimestampToDate } from "../../utils/date";
+import { formatToIDR } from "../../utils/number";
 
 function AdminOrderDetail() {
+    const {id} =  useParams();
+    const detailApi =  useGrpcApi();
+    const [apiResponse, setApiResponse] = useState<DetailOrderResponse | null>(null);
+    const items = apiResponse?.items ?? [];
+    const totalPrice = apiResponse?.total ?? 0;
+
+    useEffect (() => {
+        const fetchData = async () => {
+            const res = await detailApi.callApi(getOrderClient().detailOrder({id: id ?? ""}));
+        
+            setApiResponse(res.response);
+        }
+
+        fetchData();
+    }, []);
     return (
         <div className="admin-dashboard py-5">
             <div className="container">
                 <div className="bg-white p-4 rounded-3">
                     <div className="d-flex justify-content-between align-items-center mb-4">
-                        <h2 className="section-title">Pesanan #ORD-2025000001</h2>
-                        <Link to="/admin">
+                        <h2 className="section-title">Pesanan {apiResponse?.number ?? ""}</h2>
+                        <Link to="/admin/orders">
                             <button className="btn btn-primary">
                                 Kembali ke Pesanan
                             </button>
@@ -18,18 +40,21 @@ function AdminOrderDetail() {
                         <div className="col-md-6 mb-4">
                             <h3 className="h4 mb-3">Informasi Pelanggan</h3>
                             <div className="p-3 border rounded">
-                                <p className="mb-2"><strong>Nama:</strong> John Doe</p>
-                                <p className="mb-2"><strong>Telepon:</strong> +6281234567890</p>
-                                <p className="mb-2"><strong>Alamat:</strong> Jl. Mawar No. 123, Jakarta, Indonesia</p>
-                                <p className="mb-0"><strong>Catatan:</strong> Mohon dikirim pagi hari</p>
+                                <p className="mb-2"><strong>Nama:</strong> {apiResponse?.userFullName ?? ""}</p>
+                                <p className="mb-2"><strong>Telepon:</strong> {apiResponse?.phoneNumber ?? ""}</p>
+                                <p className="mb-2"><strong>Alamat:</strong> {apiResponse?.address ?? ""}</p>
+                                <p className="mb-0"><strong>Catatan:</strong> {apiResponse?.notes ?? ""}</p>
                             </div>
                         </div>
 
                         <div className="col-md-6 mb-4">
                             <h3 className="h4 mb-3">Status Pesanan</h3>
                             <div className="p-3 border rounded">
-                                <p className="mb-2"><strong>Status Saat Ini:</strong> <span className="badge bg-warning">Diproses</span></p>
-                                <p className="mb-2"><strong>Tanggal Pesanan:</strong> 15 Januari 2025</p>
+                                <p className="mb-2">
+                                    <strong>Status Saat Ini:</strong>
+                                <OrderStatusBadge code={apiResponse?.orderStatusCode ?? ""} />
+                                </p>
+                                <p className="mb-2"><strong>Tanggal Pesanan:</strong>{converTimestampToDate(apiResponse?.createdAt)}</p>
                                 <div className="mt-3">
                                     <select className="form-select mb-2">
                                         <option value="pending">Menunggu</option>
@@ -55,25 +80,21 @@ function AdminOrderDetail() {
                                         </tr>
                                     </thead>
                                     <tbody>
-                                        <tr>
-                                            <td>Kaos Top Up</td>
-                                            <td>Rp50.000</td>
-                                            <td>1</td>
-                                            <td>Rp50.000</td>
-                                        </tr>
-                                        <tr>
-                                            <td>Kemeja Polo</td>
-                                            <td>Rp100.000</td>
-                                            <td>1</td>
-                                            <td>Rp100.000</td>
-                                        </tr>
+                                        {items.map(item => (
+                                            <tr>
+                                            <td>{item.name}</td>
+                                            <td>{formatToIDR(item.price)}</td>
+                                            <td>{Number(item.quantity)}</td>
+                                            <td>{formatToIDR(item.price * Number(item.quantity))}</td>
+                                        </tr> 
+                                        ))}
                                         <tr>
                                             <td colSpan={3} className="text-end"><strong>Subtotal</strong></td>
-                                            <td>Rp150.000</td>
+                                            <td>{formatToIDR(totalPrice)}</td>
                                         </tr>
                                         <tr>
                                             <td colSpan={3} className="text-end"><strong>Total</strong></td>
-                                            <td><strong>Rp150.000</strong></td>
+                                            <td><strong>{formatToIDR(totalPrice)}</strong></td>
                                         </tr>
                                     </tbody>
                                 </table>
